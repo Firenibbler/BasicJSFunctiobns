963cb3ce0dac9a2ba232a4759226a2bd1e808ac1
Andrew Stavast <firenibbler@gmail.com>
2020-06-22 18:08:18 -0400

    Update Find Missing Objects in Array.js

--- Find Missing Objects in Array.js
+++ Find Missing Objects in Array.js
@@ -1,14 +1,14 @@
 // A function that finds what objects or values are missing from an array given a complete version of the array.
-function findMissing(has,shouldHave){
-  const missing=[];
-  for(let i=0;i<shouldHave.length;i++){
-    let isThere=false;
-    for(let j=0;j<has.length;j++){
-      if(shouldHave[i]===has[j]){
-        isThere=true;
+function findMissing(has, shouldHave) {
+  const missing = [];
+  for (let i = 0; i < shouldHave.length; i++) {
+    let isThere = false;
+    for (let j = 0; j < has.length; j++) {
+      if (shouldHave[i] === has[j]) {
+        isThere = true;
       }
     }
-    if(!isThere){
+    if (!isThere) {
       missing.push(shouldHave[i]);
     }
   }
